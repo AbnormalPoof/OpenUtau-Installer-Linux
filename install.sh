14405f231058f29720851dc7d87a29b70dcb2b0d
your average coder thingy idk <86753001+AbnormalPoof@users.noreply.github.com>
2022-10-04 23:02:56 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-mkdir -p ~/.local/lib
 mkdir -p ~/.local/share/pixmaps
 mkdir -p ~/.local/lib/OpenUtau
+mkdir -p ~/.local/share/applications
 echo "Downloading OpenUtau..."
-wget -q https://github.com/stakira/OpenUtau/releases/download/OpenUtau-Latest/OpenUtau-linux-x64.tar.gz -O /tmp/ou.tar.gz --show-progress 
+wget -q https://github.com/stakira/OpenUtau/releases/latest/download/OpenUtau-linux-x64.tar.gz -O /tmp/ou.tar.gz --show-progress 
 echo "Installing..."
 tar xvf /tmp/ou.tar.gz -C ~/.local/lib/OpenUtau
 echo "Retrieving icon..."
@@ -11,7 +11,7 @@ wget -q https://raw.githubusercontent.com/stakira/OpenUtau/master/Logo/openutau.
 echo "Marking executable..."
 chmod +x ~/.local/lib/OpenUtau/OpenUtau
 echo "Adding to menu..."
-wget -q https://raw.githubusercontent.com/HitCoder9768/OpenUtau-Installer-Linux/main/OpenUtau.desktop -O ~/.local/share/applications/OpenUtau.desktop --show-progress
+wget -q https://raw.githubusercontent.com/AbnormalPoof/OpenUtau-Installer-Linux/main/OpenUtau.desktop -O ~/.local/share/applications/OpenUtau.desktop --show-progress
 echo "Removing temp files..."
 rm /tmp/ou.tar.gz
 echo "Done!"
